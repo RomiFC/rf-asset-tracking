a7404ea9166926bfcdab781823b39f57c17e5db0
Remy <117617803+RomiFC@users.noreply.github.com>
2023-04-05 16:23:37 -0500

    Add files via upload

--- Capstone RFID Reader/src/main.cpp
+++ Capstone RFID Reader/src/main.cpp
@@ -8,51 +8,26 @@
 
   If using the Simultaneous RFID Tag Reader (SRTR) shield, make sure the serial slide
   switch is in the 'SW-UART' position
-*/
 
-//Used for transmitting to the device
-//If you run into compilation errors regarding this include, see the README
-#include <SoftwareSerial.h>
+  This code uses a modified version of the SparkFun_UHR_RFID_Reader library.
+  Versions downloaded online from SparkFun or the GitHub link above will not work.
+*/
 
 
 // LIBRARIES
+#include <SoftwareSerial.h>           //If you run into compilation errors regarding this include, see README
 #include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
-#include <stdio.h>
-#include <stdlib.h>
+
+// GLOBAL VARIABLES
+bool formatCheck;     //Determines if data is printed to serial monitor as csv or with labels
 
 SoftwareSerial softSerial(2, 3); //RX, TX
 RFID nano; //Create instance
 
-
-// FUNCTION PROTOTYPES
-boolean setupNano(long baudRate);
-
-
-
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial); //Wait for the serial port to come online
-
-  if (setupNano(38400) == false) //Configure nano to run at 38400bps
-  {
-    Serial.println(F("Module failed to respond. Please check wiring."));
-    while (1); //Freeze!
-  }
-
-  nano.setRegion(REGION_NORTHAMERICA); //Set to North America
-
-  nano.setReadPower(2700); //5.00 dBm. Higher values may caues USB port to brown out
-  //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
-
-  Serial.println(F("Press a key to begin scanning for tags."));
-  while (!Serial.available()); //Wait for user to send a character
-  Serial.read(); //Throw away the user's character
-
-  nano.startReading(); //Begin scanning for tags
-}
-
-void loop()
+//Prints information from scanned tags to serial monitor with relevant labels
+//This will print errors and idle notes as well, this should be used if printing to serial
+//monitor in csv format is not necessary.
+void printWithLabels()
 {
   if (nano.check() == true) //Check to see if any new data has come in from module
   {
@@ -113,12 +88,46 @@ void loop()
       //Unknown response
       Serial.print("Unknown error");
     }
-
-    
   }
 }
 
-//Gracefully handles a reader that is already configured and already reading continuously
+//Prints information from scanned tags to serial monitor in CSV format with header. 
+//This will not print errors and will remain idle if no tags are being scanned
+void printAsCSV()
+{
+  if (nano.check() == true) //Check to see if any new data has come in from module
+  {
+    byte responseType = nano.parseResponse(); //Break response into tag ID, RSSI, frequency, and timestamp
+
+    if (responseType == RESPONSE_IS_TAGFOUND)
+    {
+      //If we have a full record we can pull out the fun bits
+      int rssi = nano.getTagRSSI(); //Get the RSSI for this tag read
+      long freq = nano.getTagFreq(); //Get the frequency this tag was detected at
+      long timeStamp = nano.getTagTimestamp(); //Get the time this was read, (ms) since last keep-alive message
+      byte tagEPCBytes = nano.getTagEPCBytes(); //Get the number of bytes of EPC from response
+      long phase = nano.getTagPhase();  //Get received tag phase from 0 to 180 degrees
+
+      Serial.print(rssi);
+      Serial.print(",");
+      Serial.print(freq);
+      Serial.print(",");
+      Serial.print(timeStamp);
+      Serial.print(",");
+      for (byte x = 0 ; x < tagEPCBytes ; x++)
+      {
+        if (nano.msg[31 + x] < 0x10) Serial.print(F("0")); //Pretty print
+        Serial.print(nano.msg[31 + x], HEX);
+        Serial.print(F(" "));
+      }
+      Serial.print(",");
+      Serial.print(phase);
+      Serial.print("\n");
+    }
+  }
+}
+
+//Gracefully handles a reader that is already configured and already reading continuously. 
 //Because Stream does not have a .begin() we have to do this outside the library
 boolean setupNano(long baudRate)
 {
@@ -139,7 +148,7 @@ boolean setupNano(long baudRate)
     //This happens if the baud rate is correct but the module is doing a ccontinuous read
     nano.stopReading();
 
-    Serial.println(F("Module continuously reading. Asking it to stop..."));
+    Serial.println(F("#Module continuously reading. Asking it to stop..."));
 
     delay(1500);
   }
@@ -167,3 +176,54 @@ boolean setupNano(long baudRate)
   return (true); //We are ready to rock
 }
 
+
+// ----------------- LOOPS -----------------
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial); //Wait for the serial port to come online
+
+  if (setupNano(38400) == false) //Configure nano to run at 38400bps
+  {
+    Serial.println(F("#Module failed to respond. Please check wiring."));
+    while (1); //Freeze!
+  }
+
+  nano.setRegion(REGION_NORTHAMERICA); //Set to North America
+
+  nano.setReadPower(2700); //5.00 dBm. Higher values may caues USB port to brown out
+  //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
+
+  Serial.println("#Scan tags in datalogging format? (y/n): ");
+  while (!Serial.available());
+  byte userInput = Serial.read();
+
+  switch (userInput) {
+    case 'y':
+      formatCheck = true;
+      break;
+    case 'n':
+      formatCheck = false;
+      break;
+    default:
+      Serial.println("#Error: invalid input");
+  }
+
+  Serial.println(F("#Press a key to begin scanning for tags. If datalogging with PuTTY or other terminal emulator, run it now. "));
+  while (!Serial.available()); //Wait for user to send a character
+  Serial.read(); //Throw away the user's character
+
+  if(formatCheck) {
+    Serial.print("rssi, freq, timestamp, epc, phase\n");  //Print CSV header
+  }
+
+  nano.startReading(); //Begin scanning for tags
+}
+
+void loop() {
+  if (formatCheck)
+    printAsCSV();
+  else
+    printWithLabels();
+}
